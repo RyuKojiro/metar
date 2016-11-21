45bb42301e93b0f81c653246ca98d973c17ba710
Daniel Loffgren <daniel@loffgren.org>
2016-11-20 16:48:21 -0800

    Some curl initialization

--- src/main.c
+++ src/main.c
@@ -2,5 +2,15 @@
 #include <sysexits.h>
 
 int main(int argc, const char * const argv[]) {
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	CURL *curl = curl_easy_init();
+	if(!curl) {
+		return EX_SOFTWARE;
+	}
+
+	// Do the work
+
+	curl_easy_cleanup(curl);
+	curl_global_cleanup();
 	return EX_OK;
 }
