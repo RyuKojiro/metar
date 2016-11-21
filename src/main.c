d2fe3cd174de127a53a44fc95cef668d9ea2b31a
Daniel Loffgren <daniel@loffgren.org>
2016-11-20 20:29:03 -0800

    Also handle 3 character station ids sent to setStation

--- src/main.c
+++ src/main.c
@@ -2,9 +2,11 @@
 #include <sysexits.h> // EX_USAGE, EX_OK
 #include <err.h>      // errx, warnx
 #include <ctype.h>    // toupper, isalnum
-#include <assert.h>   // assert
+#include <string.h>   // strlen
+#include <stdbool.h>
 
-#define URL_TEMPLATE "ftp://tgftp.nws.noaa.gov/data/observations/metar/stations/XXXX.TXT"
+#define URL_TEMPLATE    "ftp://tgftp.nws.noaa.gov/data/observations/metar/stations/XXXX.TXT"
+#define STATION_ID_LEN  4
 
 union url {
 	char entirety[67];
@@ -23,11 +25,28 @@ static int usage(void) {
 	return EX_USAGE;
 }
 
-static void setStation(union url *url, const char *station) {
-	for(int i = 0; i < 4; i++) {
-		assert(isalnum(station[i]));
-		url->parts.station[i] = toupper(station[i]);
+static bool setStation(union url *url, const char *station) {
+	size_t len = strlen(station);
+	if(len != STATION_ID_LEN &&
+	   len != STATION_ID_LEN - 1) {
+		warnx("Station ID must be either three or four characters long.");
+		return false;
 	}
+
+	// Transfer the station id from end to beginning
+	for(size_t i = 1; i <= len; i++) {
+		if(!isalnum(station[len - i])) {
+			warnx("Station ID must contain only alphanumeric characters.");
+			return false;
+		}
+		url->parts.station[STATION_ID_LEN - i] = toupper(station[len - i]);
+	}
+
+	// If it was only 3 characters, prepend the 'K'
+	if (len == 3) {
+		url->parts.station[0] = 'K';
+	}
+	return true;
 }
 
 static size_t printData(void *contents, size_t size, size_t nmemb, void *userp) {
@@ -49,10 +68,14 @@ int main(int argc, const char * const argv[]) {
 
 	// Do the work
 	for (int arg = 1; arg < argc; arg++) {
-		setStation(&url, argv[arg]);
+		if(!setStation(&url, argv[arg])) {
+			continue;
+		}
+
 		curl_easy_setopt(curl, CURLOPT_URL, url.entirety);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, printData);
 		CURLcode res = curl_easy_perform(curl);
+
 		if (res != CURLE_OK) {
 			warnx("Unable to fetch information for station ID \"%s\"", argv[arg]);
 			continue;
